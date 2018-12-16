2a0aba44b7aa7f56d3f478e3274cb9320101bf38
sanjorge11 <sanjorge@live.unc.edu>
2018-12-15 22:21:40 -0500

    separate controller and module

---
+++ myApp.js
@@ -0,0 +1,7 @@
+// AngularJS modules define AngularJS applications.
+// AngularJS controllers control AngularJS applications.
+
+// The "myApp" parameter refers to an HTML element in which the
+//application will run. Now you can add controllers, directives,
+//filters, and more, to your Angular app
+var app = angular.module('myApp', []);
--- myCtrl.js
+++ myCtrl.js
@@ -1,8 +1,5 @@
 // AngularJS modules define AngularJS applications.
 // AngularJS controllers control AngularJS applications.
-
-var app = angular.module('myApp', []);
-
 app.controller('myCtrl', function($scope) {
     $scope.altf= "John";
     $scope.altl= "Doe";
