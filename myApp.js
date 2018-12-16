698ef13a5ee7e4b662298ad8242494eebc261545
sanjorge11 <sanjorge@live.unc.edu>
2018-12-16 02:15:09 -0500

    added more content

--- myApp.js
+++ myApp.js
@@ -6,3 +6,21 @@
 //filters, and more, to your Angular app
 var app = angular.module('myApp', []);
 //this is compiled AFTER the library has been loaded in script tag within head
+
+
+//note: When naming a directive, you must use a camel case name,
+//injectElement, but when invoking it, you must use - separated name,
+//inject-element
+app.directive("injectElement", function() {
+    return {
+        template : "<h1> injected from element </h1>"
+    };
+});
+
+app.directive("injectComment", function() {
+  return {
+      restrict : "M",
+      replace : true,
+      template: "<h1> injected from comment </h1>"
+  };
+});
