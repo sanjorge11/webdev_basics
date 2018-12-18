8c7fdc285dba46753351fc4ba579ab6bbd14fe3d
sanjorge11 <sanjorge@live.unc.edu>
2018-12-17 21:56:37 -0500

    started scope

--- myCtrl.js
+++ myCtrl.js
@@ -9,3 +9,13 @@ app.controller('myCtrl', function($scope) {
     };
 
 });
+
+// AngularJS will invoke the controller with a $scope object.
+// In AngularJS, $scope is the application object (the owner of application
+// variables and functions).
+
+//notice that the $scope object is available to both the view and controller
+
+//When adding properties to the $scope object in the controller,
+//the view (HTML) gets access to these properties. In the view, you
+//do not use the prefix $scope, you just refer to a propertyname, like {{var}}
