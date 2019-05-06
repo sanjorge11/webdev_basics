08320771ba9af313551ba4f1ed29d0fda698ca42
sanjorge11 <sanjorge@live.unc.edu>
2019-05-06 00:02:10 -0400

    added some comments, starting Create File with FS module

--- Nodejs/3_httpModule_2.js
+++ Nodejs/3_httpModule_2.js
@@ -9,11 +9,33 @@ http.createServer(function (req, res) {
   //parameters are: url, boolean which, if true will
   //set the .query property to an object that is returned by
   //the querystring module's .parse() method
-  var q = url.parse(req.url, true).query;
+  var q = url.parse(req.url, true)
+  console.log(q);
+
+  q = q.query;
   var txt = q.year + " " + q.month;
   res.end(txt);
 }).listen(8080);
 
+//sample querystring --> ?year=2019&month=January
+//second parameter is boolean to parse querystring, it's true
+//so the object returned will have .query property set to it
+
+//this is what the object returned by .parse() looks like
+/*Url {
+  protocol: null,
+  slashes: null,
+  auth: null,
+  host: null,
+  port: null,
+  hostname: null,
+  hash: null,
+  search: '?year=2019&month=January',
+  query: [Object: null prototype] { year: '2019', month: 'January' },
+  pathname: '/',
+  path: '/?year=2019&month=January',
+  href: '/?year=2019&month=January' }
+*/
 
 //in url after 8080, write: /?year=2017&month=July
 //this code will parse the url parameters and send a
--- Nodejs/4_fileSystem_1.js
+++ Nodejs/4_fileSystem_1.js
@@ -16,7 +16,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   //use the fs.readFile() method to read files on your computer and return the content
-  //note that we assume that demofile1.html is in same file as this nodejs app
+  //note that we assume that demofile1.html is in directory file as this nodejs app
   fs.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});  //add header to return HTML
     res.write(data);
