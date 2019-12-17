c5625281c03280a0ce30ffc1113012ef877e6e3e
Jorge S Fuentes <sanjorge.fuentes@gmail.com>
2019-12-17 00:57:07 -0500

    added root module

--- sampleWebApp/frontend/src/app/app-routing.module.ts
+++ sampleWebApp/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', component: PageNotFoundComponent }  //wildcard route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
